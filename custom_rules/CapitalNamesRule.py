ab0f2a31b2443449508e549016ca1d4e45fe354f
cristian@rincewind <cmsirbu@users.noreply.github.com>
2020-12-02 21:02:11 +0000

    Fix black formatting of files

--- custom_rules/CapitalNamesRule.py
+++ custom_rules/CapitalNamesRule.py
@@ -4,7 +4,9 @@ from ansiblelint import AnsibleLintRule
 class CapitalNamesRule(AnsibleLintRule):
     id = "289"
     shortdesc = "All task names should be capitalized"
-    description = "Using all uppercase names creates uniformity and improves readability"
+    description = (
+        "Using all uppercase names creates uniformity and improves readability"
+    )
     severity = "LOW"
     tags = ["formatting", "readability"]
     version_added = "v4.0.0"

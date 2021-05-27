1aba13182085bd6abd5a79430d44a08b8ec1ef93
cristian@rincewind <cmsirbu@users.noreply.github.com>
2021-05-27 14:27:54 +0100

    fix ansible-lint custom rule

--- custom_rules/CapitalNamesRule.py
+++ custom_rules/CapitalNamesRule.py
@@ -1,14 +1,15 @@
-from ansiblelint import AnsibleLintRule
+from ansiblelint.rules import AnsibleLintRule
 
 
 class CapitalNamesRule(AnsibleLintRule):
-    id = "289"
+    id = "CUSTOMRULE001"
     shortdesc = "All task names should be capitalized"
-    description = "Using all uppercase names creates uniformity and improves readability"
-    severity = "LOW"
+    description = (
+        "Using all uppercase names creates uniformity and improves readability"
+    )
+    severity = "HIGH"
     tags = ["formatting", "readability"]
-    version_added = "v4.0.0"
 
-    def matchtask(self, file, task):
-        if task.get("name"):
+    def matchtask(self, task, file):
+        if task.get("name") is not None:
             return task.get("name") != task.get("name").upper()

b20d3b68e63348faafcc73ebbe7036e82d952b18
Ben Marti <william.marti@gfs.com>
2022-09-01 15:29:29 -0400

    more fixes

--- example.py
+++ example.py
@@ -1,14 +1,18 @@
-# Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(
-    var_one, var_two, var_three, var_four, var_five, var_six, var_seven
-)
-
 # Further indentation required as indentation is not distinguishable.
 def long_function_name(var_one, var_two, var_three, var_four):
     print(var_one)
 
+var_one = 1
+var_two = 2
+var_three = 3
+var_four = 4
 
-result = some_function_that_takes_arguments(
+# Arguments on first line forbidden when not using vertical alignment.
+foo = long_function_name(
+    var_one, var_two, var_three, var_four
+)
+
+result = long_function_name(
     "a",
     "b",
     "c",

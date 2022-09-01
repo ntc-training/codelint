d904ae15c0a635ac154f729f784353fa67f950db
Ben Marti <william.marti@gfs.com>
2022-09-01 15:39:37 -0400

    lint fixes

--- example-black.py
+++ example-black.py
@@ -9,16 +9,9 @@ var_three = 3
 var_four = 4
 
 # Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(
-    var_one, var_two, var_three, var_four
-)
+foo = long_function_name(var_one, var_two, var_three, var_four)
 
-result = long_function_name(
-    "a",
-    "b",
-    "c",
-    "d"
-)
+result = long_function_name("a", "b", "c", "d")
 
 my_list = [
     1,

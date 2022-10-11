a89688af24ce373f5c1ad212d5e1fbcf11f2593e
siddiquee <siddiquee@ntc.com>
2022-10-11 16:02:27 +0000

    fixed pylint black pylama

--- example.py
+++ example.py
@@ -1,20 +1,35 @@
+"""Test module doc string"""
 # Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(var_one, var_two, var_three, var_four, var_five, var_six, var_seven)
-
 # Further indentation required as indentation is not distinguishable.
-def long_function_name (
-    var_one, var_two, var_three,
-    var_four):
-    print(var_one)
 
-result = some_function_that_takes_arguments(
-    'a', 'b', 'c',
-    'd', 'e', 'f',
-    )
 
-my_list = [
-    1, 2, 3,
-    4, 5, 6,
-]
+def long_function_name(var_one, var_two, var_three, var_four):
+    """To find long function name.
+    Args:
+        var_one (int): The integer value.
+        var_two (int): The integer value.
+        var_three (int): The integer value.
+        var_four (int): The integer value.
+    """
+    print(var_one, var_two, var_three, var_four)
 
-x = {  'a':37,'b':42,  'c':927}
+
+long_function_name(var_one=1, var_two=2, var_three=3, var_four=4)
+
+
+def some_function_that_takes_arguments(*args):
+    """To fix undefined variable
+    Args:
+        args: The variable arguments.
+
+    Returns:
+        list: The list of variables
+    """
+    return args
+
+
+RESULT = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f")
+
+my_list = [1, 2, 3, 4, 5, 6]
+
+x = {"a": 37, "b": 42, "c": 927}

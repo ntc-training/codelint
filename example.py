b1c4386aaac8f38853cb78f6cd82cff8b5004199
Reshi <firdousahmad.reshi@kochgs.com>
2022-10-11 20:07:40 +0530

    linting fix

--- example.py
+++ example.py
@@ -1,20 +1,41 @@
+"""Module Docstrings"""
 # Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(var_one, var_two, var_three, var_four, var_five, var_six, var_seven)
+var_one, var_two, var_three, var_four, var_five, var_six, var_seven = (
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+)
+
 
 # Further indentation required as indentation is not distinguishable.
-def long_function_name (
-    var_one, var_two, var_three,
-    var_four):
-    print(var_one)
+def long_function_name(val1, val2, val3, val4):
+    """
+    Long Function Name.
+    :param val1: value 1
+    :param val2: value 2
+    :param val3: value 3
+    :param val4: value 4
+    :return: None
+    """
+    print(val1, val2, val3, val4)
 
-result = some_function_that_takes_arguments(
-    'a', 'b', 'c',
-    'd', 'e', 'f',
-    )
 
-my_list = [
-    1, 2, 3,
-    4, 5, 6,
-]
+long_function_name(var_one, var_two, var_three, var_four)
 
-x = {  'a':37,'b':42,  'c':927}
+
+def some_function_that_takes_arguments(*kwargs):
+    """
+    Cover up functions for some function that takes arguments.
+    :param kwargs: multiple kwargs
+    :return: kwargs
+    """
+    return kwargs
+
+
+result = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f")
+my_list = [1, 2, 3, 4, 5, 6]
+x = {"a": 37, "b": 42, "c": 927}

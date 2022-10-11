e996bba4fd6a55a67efdefc51eb115bbb1df466f
Tushar Sharma <tushar.sharma@kochgs.com>
2022-10-11 12:02:40 +0000

    fixed syntax

--- example-black.py
+++ example-black.py
@@ -2,12 +2,8 @@
 def long_function_name(var_one, var_two, var_three, var_four):
     print(var_one)
 
-# Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(
-    var_one, var_two, var_three, var_four
-    )
 
-result = long_function_name("a", "b", "c", "d", "e", "f",)
+result = long_function_name("a", "b", "c", "d")
 
 my_list = [
     1,

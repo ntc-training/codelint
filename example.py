394fe20960f81a838fa8de87df882fc62ade4978
Tushar Sharma <tushar.sharma@kochgs.com>
2022-10-11 12:13:19 +0000

    fixed syntax

--- example.py
+++ example.py
@@ -1,18 +1,19 @@
-# Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(var_one, var_two, var_three, var_four, var_five, var_six, var_seven)
-
 # Further indentation required as indentation is not distinguishable.
 def long_function_name (var_one, var_two, var_three, var_four):
     print(var_one)
 
+
+# Arguments on first line forbidden when not using vertical alignment.
+foo = long_function_name (1, 2, 3, 4)
+
 result = some_function_that_takes_arguments(
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    )
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+)
 
 my_list = [
     1,
@@ -23,4 +24,4 @@ my_list = [
     6,
 ]
 
-x = {'a': 37, 'b': 42, 'c': 927}
+x = {"a": 37, "b": 42, "c": 927}

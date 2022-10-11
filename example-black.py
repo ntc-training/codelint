bc43ef2a7f66a0f1af9def1d9097628df1bcd042
firdousreshi <reshifirdousahmad@gmail.com>
2022-10-11 20:35:37 +0530

    Linting error in example file for pylama mapped same for now

--- example-black.py
+++ example-black.py
@@ -1,15 +1,4 @@
 # Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(
-    var_one, var_two, var_three, var_four, var_five, var_six, var_seven
-)
-
-# Further indentation required as indentation is not distinguishable.
-def long_function_name(var_one, var_two, var_three, var_four):
-    print(var_one)
-
-
-result = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f",)
-
 my_list = [
     1,
     2,
@@ -18,5 +7,30 @@ my_list = [
     5,
     6,
 ]
+
+var_one, var_two, var_three, var_four, var_five, var_six = my_list
+
+
+# Further indentation required as indentation is not distinguishable.
+def long_function_name(val1, val2, val3, val4):
+    """
+    Long Function name
+    :param val1:
+    :param val2:
+    :param val3:
+    :param val4:
+    :return: variable.
+    """
+    print(val1)
+
+
+long_function_name(var_one, var_two, var_three, var_four)
+
+
+def some_function_that_takes_arguments(*args):
+    return args
+
+
+result = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f")
 
 x = {"a": 37, "b": 42, "c": 927}
--- example.py
+++ example.py
@@ -1,41 +1,36 @@
-"""Module Docstrings"""
 # Arguments on first line forbidden when not using vertical alignment.
-var_one, var_two, var_three, var_four, var_five, var_six, var_seven = (
+my_list = [
     1,
     2,
     3,
     4,
     5,
     6,
-    7,
-)
+]
+
+var_one, var_two, var_three, var_four, var_five, var_six = my_list
 
 
 # Further indentation required as indentation is not distinguishable.
 def long_function_name(val1, val2, val3, val4):
     """
-    Long Function Name.
-    :param val1: value 1
-    :param val2: value 2
-    :param val3: value 3
-    :param val4: value 4
-    :return: None
+    Long Function name
+    :param val1:
+    :param val2:
+    :param val3:
+    :param val4:
+    :return: variable.
     """
-    print(val1, val2, val3, val4)
+    print(val1)
 
 
 long_function_name(var_one, var_two, var_three, var_four)
 
 
-def some_function_that_takes_arguments(*kwargs):
-    """
-    Cover up functions for some function that takes arguments.
-    :param kwargs: multiple kwargs
-    :return: kwargs
-    """
-    return kwargs
+def some_function_that_takes_arguments(*args):
+    return args
 
 
 result = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f")
-my_list = [1, 2, 3, 4, 5, 6]
+
 x = {"a": 37, "b": 42, "c": 927}

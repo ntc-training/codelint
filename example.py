8f8efe86f23926fbe5be1d10f96c37d361e76079
Tushar Sharma <tushar.sharma@kochgs.com>
2022-10-11 12:14:55 +0000

    fixed syntax

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
 # Further indentation required as indentation is not distinguishable.
-def long_function_name (var_one, var_two, var_three, var_four):
+def long_function_name(var_one, var_two, var_three, var_four):
     print(var_one)
 
 
 # Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name (1, 2, 3, 4)
+foo = long_function_name(1, 2, 3, 4)
 
-result = some_function_that_takes_arguments(
+result = long_function_name(
     "a",
     "b",
     "c",

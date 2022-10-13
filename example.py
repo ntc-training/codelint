4100a998913d6c912f9a54019c4566b95907b877
rsadhamhussain92 <sadhamhussain.raspudeen@kochgs.com>
2022-10-13 14:29:00 +0000

    final commit for pull request

--- example.py
+++ example.py
@@ -1,6 +1,10 @@
+""" Program to generate a value """
 
-def long_function_name(var_one,var_two,var_three,var_four,var_five,var_six):
-    print(var_one)
+
+def long_function_name(var_one, var_two):
+    """function to get values"""
+    print(var_one, var_two)
+
 
 result = long_function_name
-result("a","b","c","d","e","f")
+result("a", "b")

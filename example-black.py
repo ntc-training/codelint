5fece1171b50dd9b3b9b8210dd58680eb095317f
Carlos Huaccho <carlos.huaccho@gmail.com>
2023-08-31 17:24:02 +0000

    Fixed yamllint and black

--- example-black.py
+++ example-black.py
@@ -1,22 +1,23 @@
-# Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(
-    var_one, var_two, var_three, var_four, var_five, var_six, var_seven
+"""This is an example file"""
+
+
+def long_function_name(*args):
+    """Returns args"""
+    return args
+
+
+def some_function_that_takes_arguments(*args):
+    """Returns args"""
+    return args
+
+
+NEAT = long_function_name(
+    "var_one", "var_two", "var_three", "var_four", "var_five", "var_six", "var_seven"
 )
 
-# Further indentation required as indentation is not distinguishable.
-def long_function_name(var_one, var_two, var_three, var_four):
-    print(var_one)
 
+result = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f")
 
-result = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f",)
-
-my_list = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-]
+my_list = [1, 2, 3, 4, 5, 6]
 
 x = {"a": 37, "b": 42, "c": 927}

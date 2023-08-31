746d1736ba5a67b9cc947d50001aa7f0b18674db
GarrettS <garrett.skjelstad@networktocode.com>
2023-08-31 16:00:55 +0000

    GS Fixes 2

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
 # Further indentation required as indentation is not distinguishable.
-def long_func_name(var_one, var_two, var_three, var_four, var_five, var_six):
+def long_function_name(var_one, var_two, var_three, var_four, var_five, var_six):
     print(var_one)
 
 
 # Arguments on first line forbidden when not using vertical alignment.
-foo = long_func_name(
+foo = long_function_name(
     "var_one", "var_two", "var_three", "var_four", "var_five", "var_six"
 )
 
-result = long_func_name(
+result = long_function_name(
     "a",
     "b",
     "c",

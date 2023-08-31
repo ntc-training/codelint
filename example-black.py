559088de3defc9345e35899b3028584b1269307d
GarrettS <garrett.skjelstad@networktocode.com>
2023-08-31 15:59:39 +0000

    GS Fixes

--- example-black.py
+++ example-black.py
@@ -1,14 +1,21 @@
-# Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(
-    var_one, var_two, var_three, var_four, var_five, var_six, var_seven
-)
-
 # Further indentation required as indentation is not distinguishable.
-def long_function_name(var_one, var_two, var_three, var_four):
+def long_func_name(var_one, var_two, var_three, var_four, var_five, var_six):
     print(var_one)
 
 
-result = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f",)
+# Arguments on first line forbidden when not using vertical alignment.
+foo = long_func_name(
+    "var_one", "var_two", "var_three", "var_four", "var_five", "var_six"
+)
+
+result = long_func_name(
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+)
 
 my_list = [
     1,

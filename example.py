705debb475903e0c66225bfa83bff39377b1d136
Carlos Huaccho <carlos.huaccho@gmail.com>
2023-08-31 16:42:05 +0000

    Fixed errors

--- example.py
+++ example.py
@@ -1,20 +1,24 @@
+"""This is a test script we are to perform tests on"""
+
+
+def long_function_name(*args):
+    """Further indentation required as indentation is not distinguishable."""
+    return args
+
+
 # Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(var_one, var_two, var_three, var_four, var_five, var_six, var_seven)
+NEAT = long_function_name(1, 2, 3, 4, 5, 6, 7)
 
-# Further indentation required as indentation is not distinguishable.
-def long_function_name (
-    var_one, var_two, var_three,
-    var_four):
-    print(var_one)
 
-result = some_function_that_takes_arguments(
-    'a', 'b', 'c',
-    'd', 'e', 'f',
-    )
+def some_function_that_takes_arguments(*args):
+    """# This is a function that makes requirements"""
+    return args
 
-my_list = [
-    1, 2, 3,
-    4, 5, 6,
-]
 
-x = {  'a':37,'b':42,  'c':927}
+result = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f")
+
+
+my_list = [1, 2, 3, 4, 5, 6]
+
+
+x = {"a": 37, "b": 42, "c": 927}

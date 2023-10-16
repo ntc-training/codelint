989b91b4e152ae4f454bd8a0871da6d60c8ea82e
Toby Seed <tseed@tnsi.com>
2023-10-16 19:15:48 +0000

    i dont know what black doesnt like

--- example.py
+++ example.py
@@ -8,14 +8,7 @@ def long_function_name(var_one, var_two, var_three, var_four, var_five, var_six)
     print(var_six)
 
 
-my_list = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-]
+my_list = [1, 2, 3, 4, 5, 6,]
 
 # Arguments on first line forbidden when not using vertical alignment.
 foo = long_function_name(my_list[0], my_list[1], my_list[2], my_list[3], my_list[4], my_list[5])

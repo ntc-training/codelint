07d293a01edc4e07e306aa1a95b6849a28225a66
Kirstin Nickerson <knickerson@tnsi.com>
2023-10-16 20:00:43 +0000

    KN: attempting fixes 8

--- example-black.py
+++ example-black.py
@@ -8,6 +8,7 @@ var_five = "bluh"
 var_six = "blyh"
 var_seven = "blaeiouyh"
 
+
 # Further indentation required as indentation is not distinguishable.
 def long_function_name(var_one, var_two, var_three, var_four):
     print(var_one)
@@ -17,6 +18,7 @@ def long_function_name(var_one, var_two, var_three, var_four):
 foo = long_function_name(
     var_one, var_two, var_three, var_four, var_five, var_six, var_seven
 )
+
 
 def some_function_that_takes_arguments(var_one, var_two, var_three, var_four, var_five, var_six):
     print(var_one)
--- example.py
+++ example.py
@@ -8,6 +8,7 @@ var_five = "bluh"
 var_six = "blyh"
 var_seven = "blaeiouyh"
 
+
 # Further indentation required as indentation is not distinguishable.
 def long_function_name(var_one, var_two, var_three, var_four):
     print(var_one)
@@ -15,6 +16,7 @@ def long_function_name(var_one, var_two, var_three, var_four):
 
 # Arguments on first line forbidden when not using vertical alignment.
 foo = long_function_name(var_one, var_two, var_three, var_four, var_five, var_six, var_seven)
+
 
 def some_function_that_takes_arguments(var_one, var_two, var_three, var_four, var_five, var_six):
     print(var_one)

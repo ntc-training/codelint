904f7921ffda99a38ccc815e6f43b373249d7cbb
Kirstin Nickerson <knickerson@tnsi.com>
2023-10-16 18:36:00 +0000

    KN: attempting fixes 3

--- example-black.py
+++ example-black.py
@@ -1,16 +1,15 @@
 #! /usr/bin/env python
 
+# Further indentation required as indentation is not distinguishable.
+def long_function_name(var_one, var_two, var_three, var_four):
+    print(var_one)
+
 # Arguments on first line forbidden when not using vertical alignment.
 foo = long_function_name(
     var_one, var_two, var_three, var_four, var_five, var_six, var_seven
 )
 
-# Further indentation required as indentation is not distinguishable.
-def long_function_name(var_one, var_two, var_three, var_four):
-    print(var_one)
-
-
-result = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f",)
+result = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f")
 
 my_list = [
     1,
@@ -18,7 +17,7 @@ my_list = [
     3,
     4,
     5,
-    6,
+    6
 ]
 
 x = {"a": 37, "b": 42, "c": 927}

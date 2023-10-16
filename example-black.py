da1140445d4f95382615a6605a27e479c90bb5f8
Kirstin Nickerson <knickerson@tnsi.com>
2023-10-16 18:51:54 +0000

    KN: attempting fixes 6

--- example-black.py
+++ example-black.py
@@ -1,5 +1,13 @@
 #! /usr/bin/env python
 
+var_one = "blah"
+var_two = "bleh"
+var_three = "blih"
+var_four = "bloh"
+var_five = "bluh"
+var_six = "blyh"
+var_seven = "blaeiouyh"
+
 # Further indentation required as indentation is not distinguishable.
 def long_function_name(var_one, var_two, var_three, var_four):
     print(var_one)
@@ -8,6 +16,9 @@ def long_function_name(var_one, var_two, var_three, var_four):
 foo = long_function_name(
     var_one, var_two, var_three, var_four, var_five, var_six, var_seven
 )
+
+def some_function_that_takes_arguments(var_one, var_two, var_three, var_four, var_five, var_six):
+  print(var_one)
 
 result = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f")
 
@@ -20,4 +31,4 @@ my_list = [
     6
 ]
 
-x = { "a": 37, "b": 42, "c": 927 }
+x = {"a": 37, "b": 42, "c": 927}
--- example.py
+++ example.py
@@ -1,11 +1,22 @@
 #! /usr/bin/env python
 
+var_one = "blah"
+var_two = "bleh"
+var_three = "blih"
+var_four = "bloh"
+var_five = "bluh"
+var_six = "blyh"
+var_seven = "blaeiouyh"
+
 # Further indentation required as indentation is not distinguishable.
 def long_function_name(var_one, var_two, var_three, var_four):
     print(var_one)
 
 # Arguments on first line forbidden when not using vertical alignment.
 foo = long_function_name(var_one, var_two, var_three, var_four, var_five, var_six, var_seven)
+
+def some_function_that_takes_arguments(var_one, var_two, var_three, var_four, var_five, var_six):
+  print(var_one)
 
 result = some_function_that_takes_arguments(
     'a', 'b', 'c', 'd', 'e', 'f',
@@ -15,4 +26,4 @@ my_list = [
     1, 2, 3, 4, 5, 6,
 ]
 
-x = { 'a': 37,'b': 42, 'c': 927 }
+x = {'a': 37, 'b': 42, 'c': 927}

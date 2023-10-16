2eba85699ba67b25fdcd11cab20b215d9194ba82
Bob <bob@example.com>
2023-10-16 18:09:35 +0000

    black sorted it for me

--- example-black.py
+++ example-black.py
@@ -3,12 +3,20 @@ foo = long_function_name(
     var_one, var_two, var_three, var_four, var_five, var_six, var_seven
 )
 
+
 # Further indentation required as indentation is not distinguishable.
 def long_function_name(var_one, var_two, var_three, var_four):
     print(var_one)
 
 
-result = some_function_that_takes_arguments("a", "b", "c", "d", "e", "f",)
+result = some_function_that_takes_arguments(
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+)
 
 my_list = [
     1,
--- example.py
+++ example.py
@@ -1,20 +1,30 @@
 # Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(var_one, var_two, var_three, var_four, var_five, var_six, var_seven)
+foo = long_function_name(
+    var_one, var_two, var_three, var_four, var_five, var_six, var_seven
+)
+
 
 # Further indentation required as indentation is not distinguishable.
-def long_function_name (
-    var_one, var_two, var_three,
-    var_four):
+def long_function_name(var_one, var_two, var_three, var_four):
     print(var_one)
 
+
 result = some_function_that_takes_arguments(
-    'a', 'b', 'c',
-    'd', 'e', 'f',
-    )
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+)
 
 my_list = [
-    1, 2, 3,
-    4, 5, 6,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
 ]
 
-x = {  'a':37,'b':42,  'c':927}
+x = {"a": 37, "b": 42, "c": 927}

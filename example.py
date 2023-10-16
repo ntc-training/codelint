f95088dedfd53cf09b61caeb51100bdd768b8da8
Kirstin Nickerson <knickerson@tnsi.com>
2023-10-16 18:43:06 +0000

    KN: attempting fixes 5

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Further indentation required as indentation is not distinguishable.
-def long_function_name (var_one, var_two, var_three, var_four):
+def long_function_name(var_one, var_two, var_three, var_four):
     print(var_one)
 
 # Arguments on first line forbidden when not using vertical alignment.
@@ -15,4 +15,4 @@ my_list = [
     1, 2, 3, 4, 5, 6,
 ]
 
-x = {  'a':37,'b':42,  'c':927}
+x = { 'a': 37,'b': 42, 'c': 927 }

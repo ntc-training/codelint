1310ba3623152572d3b5991fb06fed6443206004
Austin Uecker <austin.uecker@cox.com>
2023-10-30 20:53:22 +0000

    try to fix for real

--- example.py
+++ example.py
@@ -1,19 +1,38 @@
 # Arguments on first line forbidden when not using vertical alignment.
-# foo = long_function_name(var_one, var_two, var_three, var_four, var_five, var_six, var_seven)
 
-# # Further indentation required as indentation is not distinguishable.
-# def long_function_name(
-#     var_one, var_two, var_three,
-#     var_four):
-#         print(var_one)
 
-# result = some_function_that_takes_arguments('a', 'b', 'c', 'd', 'e', 'f',)
+# Further indentation required as indentation is not distinguishable.
+def long_function_name (
+    var_one, var_two, var_three,
+    var_four):
+    print(var_one)
 
-# my_list = [
-#     1, 2, 3,
-#     4, 5, 6,
-# ]
+foo = long_function_name(
+    var_one,
+    var_two,
+    var_three,
+    var_four,
+    var_five,
+    var_six,
+    var_seven
+    )
 
-# x = { 'a': 37, 'b': 42, 'c': 927}
+result = some_function_that_takes_arguments(
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f'
+    )
 
-print("I need to learn python better")
+my_list = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6
+    ]
+
+x = {'a': 37,'b': 42, 'c': 927}

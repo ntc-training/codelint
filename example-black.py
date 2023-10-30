6d617223596fd262c275de371eda6b79cffe76f0
Austin Uecker <austin.uecker@cox.com>
2023-10-30 20:33:31 +0000

    fix indentation

--- example-black.py
+++ example-black.py
@@ -3,7 +3,7 @@
 
 # Further indentation required as indentation is not distinguishable.
 def long_function_name(var_one, var_two, var_three, var_four):
-print(var_one)
+        print(var_one)
 
 # foo = long_function_name(
 #    var_one, var_two, var_three, var_four, var_five, var_six, var_seven
